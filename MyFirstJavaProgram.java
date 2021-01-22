84da52b99ca3c98ca487275a09aee052ed9d50a9
davvvidy <przemekdawidowski97@gmail.com>
2021-01-22 10:33:44 +0100

    First exercise

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,14 @@
+public class MyFirstJavaProgram {
+
+    public static void main(String[] args) {
+        char nameFirstLetter = 'P';
+        byte age = 24;
+        short year = 2021;
+        float todaysTemperature = (float) 4.3;
+        byte biggerNumber = (byte) 200;
+        System.out.println("My first letter of name is " + nameFirstLetter);
+        System.out.println("In " + year + ", I'm " + age + " years old");
+        System.out.println("The temperature today is " + todaysTemperature);
+        System.out.println("This number will show incorrect: " + biggerNumber);
+    }
+}
